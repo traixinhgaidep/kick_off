a9329812fe166bf8288f75c256fa13a806481adb
PHONGNV-D0\phongnv <phongnv@rikkeisoft.com>
2018-05-17 17:19:33 +0700

    reffactor code

--- ServerSite/ServerSite/ActionFilters/RbacAuthorizeAttribute.cs
+++ ServerSite/ServerSite/ActionFilters/RbacAuthorizeAttribute.cs
@@ -32,20 +32,22 @@ namespace ServerSite.ActionFilters
 
                 if (!requestingUser.HasPermission(requiredPermission) & !requestingUser.IsSysAdmin)
                 {
-                    string url = string.Format("{0}://{1}:{2}", actionContext.Request.RequestUri.Scheme, actionContext.Request.RequestUri.Host, actionContext.Request.RequestUri.Port);
-                    var response = actionContext.Request.CreateResponse(HttpStatusCode.Redirect);
-                    response.Headers.Location = new Uri(url + "/api/unauthorised");
-                    actionContext.Response = response;
+                    RedirectUrl(actionContext);
                 }
             }
             else
             {
-                string url = string.Format("{0}://{1}:{2}", actionContext.Request.RequestUri.Scheme, actionContext.Request.RequestUri.Host, actionContext.Request.RequestUri.Port);
-                var response = actionContext.Request.CreateResponse(HttpStatusCode.Redirect);
-                response.Headers.Location = new Uri(url + "/api/unauthorised");
-                actionContext.Response = response;
+                RedirectUrl(actionContext);
             }
 
+        }
+
+        private void RedirectUrl(HttpActionContext actionContext)
+        {
+            string url = string.Format("{0}://{1}:{2}", actionContext.Request.RequestUri.Scheme, actionContext.Request.RequestUri.Host, actionContext.Request.RequestUri.Port);
+            var response = actionContext.Request.CreateResponse(HttpStatusCode.Redirect);
+            response.Headers.Location = new Uri(url + "/api/unauthorised");
+            actionContext.Response = response;
         }
     }
 
